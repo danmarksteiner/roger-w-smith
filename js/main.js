17e2799f23df3a33cde05140c0d2bbc04bad4c9d
danmarksteiner <marksteiner.dan@gmail.com>
2021-10-14 09:36:10 +0100

    Media gallery updates

--- js/main.js
+++ js/main.js
@@ -765,7 +765,7 @@ $(document).ready(function() {
                 image: {
                     tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
                     titleSrc: function(item) {
-                        return item.el.attr('title');
+                        return item.el.attr('hoverTitle');
                     }
                 }
             });
